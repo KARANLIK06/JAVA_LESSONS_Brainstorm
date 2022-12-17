ff8b940096b92b135c60130ef34c3dbc59310316
Ali KARANLIK <alikaranlik3112@gmail.com>
2022-12-17 14:24:22 +0300

    day39_Recap

---
+++ src/day39_Recap/shapeTask/Circle.java
@@ -0,0 +1,19 @@
+package day39_Recap.shapeTask;
+
+public class Circle {
+}
+/*
+Circle extends Shape:
+		variables:
+			radius
+			pi (static)
+
+		Encapsulate the field
+
+		Add a constructor to set the filed
+
+		area(): radius * radius * pi
+		perimeter(): 2 * radius * pi
+		toString(): r, pi, area, perimeter
+
+ */
