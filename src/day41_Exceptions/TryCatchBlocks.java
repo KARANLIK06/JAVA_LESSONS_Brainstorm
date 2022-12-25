3440f9dc1c996733a87437e66be8f2846a486433
Ali KARANLIK <alikaranlik3112@gmail.com>
2022-12-25 13:16:58 +0300

    day41_Exceptions

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,64 +1,95 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
     public static void main(String[] args) {
-        System.out.println("Test1 Started");
+
+        System.out.println("Test1 started");
 
         try{
+
             System.out.println(9/0);
+
             System.out.println("Try Block");
 
-        }catch (ArithmeticException a){
+        }catch (ArithmeticException e){
+
             System.out.println("Catch Block");
-            System.out.println("Arithmetic Exception is was occurred");
+
+            System.out.println("Arithmetic Exception was occurred");
+
         }
+
 
 
 
         System.out.println("Test1 Completed");
-        System.out.println("=================================");
 
+        System.out.println("------------------------------");
 
-        System.out.println("Test2 Started");
+        System.out.println("Test2 started");
 
-        int[] numbers ={1,2,3,4,5};
+        int[] numbers = {1,2,3,4,5};
 
-        try{
+        try {
 
             System.out.println(numbers[200]);
+
             System.out.println("Try Block");
 
-        }catch (RuntimeException e){  // Parent class( RuntimeException) can also happen
-/*
-            System.out.println("Catch Block");
-            System.out.println("ArrayIndexOutOfBounds Exception is was occurred");
+        }catch (RuntimeException e){
 
- */
-              e.printStackTrace();
-            //System.out.println(e.getMessage());
+            e.printStackTrace();
+
+            // System.out.println( e.getMessage() );
         }
-
 
         System.out.println("Test2 Completed");
 
-        System.out.println("=================================");
+        System.out.println("--------------------------------------------------");
+
+        System.out.println("Test3 started");
 
 
-        System.out.println("Test3 Started");
-
-        try{
+        try {
             System.out.println("Cydeo".substring(2, 0));
         }catch (RuntimeException e){
             e.printStackTrace();
         }
+
         System.out.println("Test3 Completed");
 
-        System.out.println("=================================");
+
+        System.out.println("--------------------------------------------------");
 
 
-        System.out.println("Test4 Started");
+        System.out.println("Hello");
 
+
+        try {
+
+            Thread.sleep(3000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Cydeo");
+
+
+        System.out.println("------------------------------------------");
+
+
+        try {
+            FileInputStream file = new FileInputStream("File path");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
     }
+
 }
