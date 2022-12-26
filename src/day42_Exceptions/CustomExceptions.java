4b7a0e5a97272771183d01a2e8dc055ee4f4f461
Ali KARANLIK <alikaranlik3112@gmail.com>
2022-12-26 22:44:42 +0300

    day42_Exceptions

---
+++ src/day42_Exceptions/CustomExceptions.java
@@ -0,0 +1,28 @@
+package day42_Exceptions;
+class FadyException extends RuntimeException{       //  custom unchecked exception
+    public FadyException() {
+        super("It's time for short break");
+    }
+
+    public FadyException(String message){
+        super(message);
+    }
+}
+
+class NoBreakException extends Exception{       //  custom checked exception
+
+}
+
+public class CustomExceptions {
+    public static void main(String[] args)  {
+      // throw new FadyException("It's time for lunch break");
+
+        try {
+            throw new NoBreakException();
+        } catch (NoBreakException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
