61cd89dbdecd19cc81812de50f99caedefb511b5
Ali KARANLIK <alikaranlik3112@gmail.com>
2022-12-26 22:19:25 +0300

    day42_Exceptions

--- src/day42_Exceptions/ThrowKeyword2.java
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -5,6 +5,5 @@ import java.util.NoSuchElementException;
 public class ThrowKeyword2 {
     public static void main(String[] args) {
        throw  new NoSuchElementException("No such element");   //  must be unchecked exceptions
-
     }
 }
