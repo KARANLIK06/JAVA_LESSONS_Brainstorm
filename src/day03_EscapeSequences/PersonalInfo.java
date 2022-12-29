a0a746b9163277425ea20e5881e058337ec5f8c1
Ali KARANLIK <alikaranlik3112@gmail.com>
2022-12-29 23:14:12 +0300

    day03_EscapeSequences

---
+++ src/day03_EscapeSequences/PersonalInfo.java
@@ -0,0 +1,22 @@
+package day03_EscapeSequences;
+
+public class PersonalInfo {
+    /*
+    1. Create a class called PersonalInfo and print followings:
+						name
+                        gender
+                        date of birth
+                        address
+                        phone number
+                        SSN
+
+     */
+    public static void main(String[] args) {
+        System.out.println("name");
+        System.out.println("gender");
+        System.out.println("date of birth");
+        System.out.println("address");
+        System.out.println("phone number");
+        System.out.println("SSN");
+    }
+}
---
+++ src/day03_EscapeSequences/Triangle.java
@@ -0,0 +1,23 @@
+package day03_EscapeSequences;
+
+public class Triangle {
+    /*
+    2. Create a class called Triangle.java and print:
+
+                    ^
+                   / \
+                  /   \
+                 /     \
+                /       \
+                ---------
+     */
+    public static void main(String[] args) {
+        System.out.println("    ^");
+        System.out.println("   / \\");
+        System.out.println("  /   \\");
+        System.out.println(" /     \\");
+        System.out.println("/       \\");
+        System.out.println("---------");
+
+    }
+}
