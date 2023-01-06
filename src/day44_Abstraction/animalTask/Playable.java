0171c4601f39ec72d9b6611e22c254bffb13b2a1
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-06 21:06:51 +0300

    day44_Abstraction_interface

---
+++ src/day44_Abstraction/animalTask/Playable.java
@@ -0,0 +1,19 @@
+package day44_Abstraction.animalTask;
+
+public interface Playable {
+
+  boolean isFriendly=true;  //  static & final by default
+
+  /*
+  public static void methot1(){
+    System.out.println(isFriendly);
+  }
+   */
+
+
+
+
+  void play();    //  Abstract by default
+
+
+}
