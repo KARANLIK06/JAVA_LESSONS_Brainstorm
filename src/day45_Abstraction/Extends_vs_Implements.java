905daf234bfeca969a595d31a6a13c6f61bffffe
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-07 15:29:55 +0300

    day45_Abstraction.Extends_vs_Implements

---
+++ src/day45_Abstraction/Extends_vs_Implements.java
@@ -0,0 +1,43 @@
+package day45_Abstraction;
+
+
+    //-------------class extends----------------
+    class C {
+    }
+
+    class A {
+
+    }
+
+    class B extends A {
+
+    }
+
+    // -------------interface extends-------------
+    interface Z {
+
+    }
+
+    interface X {
+
+    }
+
+    interface Y extends X, Z {
+
+    }
+
+
+//implements
+
+    class D implements X, Y, Z {
+
+    }
+
+
+
+    public class Extends_vs_Implements extends A implements X, Y, Z {
+    }
+
+
+
+
