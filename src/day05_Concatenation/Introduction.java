60f8c1e1012cd7fc0b6b06886e19e4bd1b9d17bf
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 12:28:10 +0300

    day05_Concatenation

---
+++ src/day05_Concatenation/Introduction.java
@@ -0,0 +1,10 @@
+package day05_Concatenation;
+
+public class Introduction {
+    public static void main(String[] args) {
+        String name = "jimmy";
+        int age = 29;
+        System.out.println("Hello Everyone,My name is " + name + ",and I am " + age + " years old.");
+    }
+
+}
