311b83f9be5b8e0c0767245d374c07b5db16b0a4
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 12:26:13 +0300

    day04_Variables

---
+++ src/day04_Variables/PrimitivesIntro.java
@@ -0,0 +1,20 @@
+package day04_Variables;
+
+public class PrimitivesIntro {
+    public static void main(String[] args) {
+        // age: 35 years old.
+        byte age = 35;
+        float tax = 0.26f;//float değerin sonuna F veya f yazılır.
+        long sallry=1000L;
+        char gender='M';//Char tek karakterden oluşur.tek tırnak 'A' içine yazılır.
+        boolean isMarried= true;//boolean sadece true/false değeri döndürür.
+        boolean compare= 10<15;// 10 değeri 15 den küçük olduğu için true döndürür.
+        System.out.println("compare = " + compare);
+        String text ="Nikontaş Kontrplak";
+        System.out.println("text = " + text);
+
+
+
+
+    }
+}
