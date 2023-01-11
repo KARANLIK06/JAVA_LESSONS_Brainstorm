46eb2524dcedf48f8377346e9e960bd606124cfa
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 12:25:31 +0300

    day04_Variables

---
+++ src/day04_Variables/EmployeeInfo.java
@@ -0,0 +1,34 @@
+package day04_Variables;
+
+public class EmployeeInfo {
+    public static void main(String[] args) {
+
+
+        String name = "Ali";
+        int age = 35;
+        char gender = 'M';
+        String company = "Nikontaş Plywood Factory";
+        String jobTitle = "dfdsfs";
+        int salary = 5500;
+        double yearsOfExpereince = 1.5;
+        boolean isFullTime = true;
+        boolean isMarried = true;
+        String employeeId = "A1";
+        String ssn = "0123_456_78_90";
+
+        System.out.println("name                =    " + name);
+        System.out.println("age                 =    " + age);
+        System.out.println("gender              =    " + gender);
+        System.out.println("company             =    " + company);
+        System.out.println("jobTitle            =    " + jobTitle);
+        System.out.println("salary              =    " + salary);
+        System.out.println("yearsOfExpereince   =    " + yearsOfExpereince);
+        System.out.println("isFullTime          =    " + isFullTime);
+        System.out.println("isMarried           =    " + isMarried);
+        System.out.println("employeeId          =    " + employeeId);
+        System.out.println("ssn                 =    " + ssn);
+
+    }
+
+
+}
