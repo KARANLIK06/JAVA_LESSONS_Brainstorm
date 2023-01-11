41a2ef4253fd05f84940f95313274d84ffbeb041
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 12:28:00 +0300

    day05_Concatenation

---
+++ src/day05_Concatenation/FullName.java
@@ -0,0 +1,11 @@
+package day05_Concatenation;
+
+public class FullName {
+    public static void main(String[] args) {
+        String firstName = "Ali";
+        String lastName = "KARANLIK";
+
+        System.out.println("fullName = " + firstName + " " + lastName);
+
+    }
+}
