d1b297c77aea2f8489196b2c8e1ec7b09afc7cc0
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 12:25:58 +0300

    day04_Variables

---
+++ src/day04_Variables/KgToPounds.java
@@ -0,0 +1,18 @@
+package day04_Variables;
+
+public class KgToPounds {
+    public static void main(String[] args) {
+        /*
+        Create a class named KgToPounds, write a program that can convert kg (int) to pounds (double)
+
+					Hints: 1 kg = 2.2 pounds
+
+         */
+        int kg = 10;
+        double pounds = 2.2 * kg;
+
+        System.out.println("pounds = " + pounds);
+        System.out.println("kg = " + kg);
+
+    }
+}
