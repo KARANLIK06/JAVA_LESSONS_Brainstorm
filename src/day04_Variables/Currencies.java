9af87206c1bfac6bd632d0160d2488c0134cf68a
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 12:25:25 +0300

    day04_Variables

---
+++ src/day04_Variables/Currencies.java
@@ -0,0 +1,14 @@
+package day04_Variables;
+
+public class Currencies {
+    public static void main(String[] args) {
+
+        int dollar = 1000;
+        double lira = dollar * 18.45;
+        double euro = dollar * 1.05;
+
+        System.out.println("euro = " + euro);
+        System.out.println("lira = " + lira);
+        System.out.println("dollar = " + dollar);
+    }
+}
