aa233b0a1f971d93a2cabda24b10bede058f49a2
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-12 21:07:06 +0300

    day06_PrimitiveTypeCastings

---
+++ src/day06_PrimitiveTypeCastings/ArithmeticOperators.java
@@ -0,0 +1,27 @@
+package day06_PrimitiveTypeCastings;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        System.out.println("12"+1); //Concatenation
+        System.out.println(10+20);  //Addition
+        System.out.println(50-40);  //Subtract
+        System.out.println(6*10);   //Multiplication
+        System.out.println(10/3);  //Division
+        System.out.println(10.0/4);
+        System.out.println(10d/4);
+        
+        int a =100;
+        double b=a/6;
+        System.out.println("b = " + b);
+
+        double c = b/6;
+        System.out.println("c = " + c);
+
+        System.out.println(100d);
+
+
+
+
+
+    }
+}
