cc9993f58e1eec3f0804306f47601ca112fb20a9
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-21 08:36:50 +0300

    day09_IfStatements

---
+++ src/day09_IfStatements/PosNegZero.java
@@ -0,0 +1,28 @@
+package day09_IfStatements;
+
+public class PosNegZero {
+    public static void main(String[] args) {
+        int n = 100;
+        if (n > 0) {
+            System.out.println("Positive");
+        } else if (n < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+        System.out.println("----------------------");
+
+        boolean positive = n > 0;
+        boolean negative = n < 0;
+
+
+        if (positive) {
+            System.out.println("Positive");
+        } else if (negative) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+
+    }
+}
