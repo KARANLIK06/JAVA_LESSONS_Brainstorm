cae05774b45017e7cb3cfef0612b191f32b8987b
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-21 12:11:49 +0300

    day11_Switch_Scanner

---
+++ src/day11_Switch_Scanner/Calculator.java
@@ -0,0 +1,31 @@
+package day11_Switch_Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        double n1 = 200.5;
+        double n2 = 50.0;
+        char operator = '/';
+
+        boolean valid = operator == '+' || operator == '-' || operator == '*' || operator == '/';
+        if (valid) {
+
+            switch (operator) {
+                case '+':
+                    System.out.println(n1 + n2);
+                    break;
+                case '-':
+                    System.out.println(n1 - n2);
+                    break;
+                case '*':
+                    System.out.println(n1 * n2);
+                    break;
+                default:
+                    System.out.println(n1 / n2);
+
+
+            }
+        }else {
+            System.out.println("Invalid Operator" + operator);
+        }
+    }
+}
