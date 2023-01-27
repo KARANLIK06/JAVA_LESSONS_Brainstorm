4fd48364753c5b28044feb725476e23b24ab5369
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-27 23:21:08 +0300

    day15_ForLoop

---
+++ src/day15_ForLoop/FizzBuzz.java
@@ -0,0 +1,25 @@
+package day15_ForLoop;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int n = scan.nextInt();
+
+        for (int i=1; i<=n; i++){
+            if (i%15==0){
+                System.out.print("FizzBuzz ");
+            } else if (i%3==0) {
+                System.out.print("Fizz ");
+            } else if (i%5==0) {
+                System.out.print("Buzz ");
+            }else {
+                System.out.print(i+" ");
+            }
+        }
+        scan.close();
+    }
+
+}
