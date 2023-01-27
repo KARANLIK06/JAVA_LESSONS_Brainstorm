e865cc8734aca17d817a3a1afa11b71d60844275
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-27 23:21:21 +0300

    day15_ForLoop

---
+++ src/day15_ForLoop/ForLoopPractices.java
@@ -0,0 +1,31 @@
+package day15_ForLoop;
+
+public class ForLoopPractices {
+    public static void main(String[] args) {
+        for (int i=15; i<=45; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("--------------");
+
+        for (int j=100; j>=50; j--){
+                System.out.print(j+" ");
+        }
+        System.out.println();
+        System.out.println("----------");
+
+        //print all the even number between 1 ~ 55
+        for (int i =1; i<=55; i++){
+            if (i%2==0){
+                System.out.print(i+" ");
+            }
+
+        }
+
+        /*for (int i = 2; i<=55; i+=2){
+            System.out.print(i+" ");
+        }
+
+         */
+    }
+}
