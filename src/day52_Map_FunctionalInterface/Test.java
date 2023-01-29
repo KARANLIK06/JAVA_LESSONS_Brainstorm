f79092791cf2bb40fb92efbe29920ee3a5e1093b
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-29 15:00:34 +0300

    day52_Map_FunctionalInterface_Tests

--- src/day52_Map_FunctionalInterface/Test.java
+++ src/day52_Map_FunctionalInterface/Test.java
@@ -1,16 +1,19 @@
 package day52_Map_FunctionalInterface;
 
+// lambda:  () -> {}
 public class Test {
     public static void main(String[] args) {
         // function1: create a function that can display a number is odd or even
+        MyFirstFunctionalInterface oddOrEvenNumber = (n) -> {
+            if (n % 2 == 0) {
+                System.out.println(n + " is even number");
+            } else {
+                System.out.println(n + " is odd number");
+            }
+
+        };
+        oddOrEvenNumber.apply(20);
 
 
-        // function2: create a function that can check if a person is eligible to buy alcohol
-
-
-        //function3: create a function that can display the cube of a number
-
-
-        //function4: create a function that can check if a number is evenly divisble by  3 & 5
     }
 }
