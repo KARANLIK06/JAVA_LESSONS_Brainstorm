d27eff504420862dcb2ad5c106257f5d6caf5cb5
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-29 15:17:33 +0300

    day52_Map_FunctionalInterface_Tests

--- src/day52_Map_FunctionalInterface/Test2.java
+++ src/day52_Map_FunctionalInterface/Test2.java
@@ -9,6 +9,12 @@ public class Test2 {
         };
         printEach.test("Wooden Spoon");
 
+        System.out.println("--------------------------------");
+
+        MySecondFunctionalInterface<Integer> cube = n -> System.out.println(n*n*n);    // Bu şekilde de kullanılabiliyor.
+
+        cube.test(5);
+
 
     }
 }
