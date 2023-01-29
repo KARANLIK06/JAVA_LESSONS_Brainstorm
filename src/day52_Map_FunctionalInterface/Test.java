f96efda8293cd566ceb8030613d76af4958a9044
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-29 14:30:29 +0300

    day52_Map_FunctionalInterface_Tests

---
+++ src/day52_Map_FunctionalInterface/Test.java
@@ -0,0 +1,16 @@
+package day52_Map_FunctionalInterface;
+
+public class Test {
+    public static void main(String[] args) {
+        // function1: create a function that can display a number is odd or even
+
+
+        // function2: create a function that can check if a person is eligible to buy alcohol
+
+
+        //function3: create a function that can display the cube of a number
+
+
+        //function4: create a function that can check if a number is evenly divisble by  3 & 5
+    }
+}
