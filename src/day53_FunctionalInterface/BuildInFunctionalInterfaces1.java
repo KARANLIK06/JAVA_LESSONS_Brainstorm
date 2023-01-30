ecb1697c25d6d040e68703b6d1f09997d2df8d47
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-30 21:03:47 +0300

    day53_BuildInFunctionalInterfaces1

--- src/day53_FunctionalInterface/BuildInFunctionalInterfaces1.java
+++ src/day53_FunctionalInterface/BuildInFunctionalInterfaces1.java
@@ -40,5 +40,20 @@ public class BuildInFunctionalInterfaces1 {
         printEach.accept("Java");
 
 
+        System.out.println("-------------------------------");
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        /*
+        for (Integer each : list2) {
+            if (each%2!=0){
+                System.out.println(each);
+            }
+        }
+
+         */
+        list2.forEach(p -> {if(p%2!=0) System.out.println(p);});
+
+
+
     }
 }
