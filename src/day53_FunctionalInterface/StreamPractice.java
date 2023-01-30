41b988a49047e9438ddf4f7dc60f84bea197a476
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-30 23:40:56 +0300

    day53_StreamPractice

--- src/day53_FunctionalInterface/StreamPractice.java
+++ src/day53_FunctionalInterface/StreamPractice.java
@@ -11,14 +11,14 @@ public class StreamPractice {
 
         list1 = list1.stream().distinct().collect(Collectors.toList());
 
-        System.out.println(list1);
+        System.out.println(list1);  //  [1, 2, 3, 4, 5, 6]
 
         System.out.println("------------------------------------");
 
         int[] arr1 = {1,1,2,2,3,3,4,4,5,5,6,6};
         arr1 = Arrays.stream(arr1).distinct().toArray();
 
-        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr1));  //  [1, 2, 3, 4, 5, 6]
 
         System.out.println("------------------------------------");
 
@@ -26,28 +26,28 @@ public class StreamPractice {
 
         list2=list2.stream().skip(5).collect(Collectors.toList());
 
-        System.out.println(list2);
+        System.out.println(list2);  //  [6, 7, 8, 9, 10]
 
         System.out.println("------------------------------------");
 
         int[] nums2 =  {1,2,3,4,5,6,7,8,9,10};
         nums2 = Arrays.stream(nums2).skip(4).toArray();
 
-        System.out.println(Arrays.toString(nums2));
+        System.out.println(Arrays.toString(nums2));     //  [5, 6, 7, 8, 9, 10]
 
         System.out.println("------------------------------------");
 
         List<Integer> list3 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
         list3 = list3.stream().limit(7).skip(3).collect(Collectors.toList());
 
-        System.out.println(list3);
+        System.out.println(list3);  //  [4, 5, 6, 7]
 
         System.out.println("------------------------------------");
 
         List<Integer> list4 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
         list4 = list4.stream().map( p -> p * 10 ).collect(Collectors.toList());
 
-        System.out.println(list4);
+        System.out.println(list4);  //  [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
         System.out.println("------------------------------------");
 
@@ -55,7 +55,15 @@ public class StreamPractice {
 
         days =  days.stream().map( p -> p.substring(0, 3) ).collect(Collectors.toList());
 
-        System.out.println(days);
+        System.out.println(days);   //  [Mon, Tue, Wed, Thu, Fri, Sat, Sun]
+
+        System.out.println("------------------------------------");
+
+        List<Integer> list5 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        List<Integer> evens = list5.stream().filter( p -> p%2==0).collect(Collectors.toList());
+
+        System.out.println(evens);      // [2, 4, 6, 8, 10]
+
 
     }
 }
