dd20431b69a0775ea3648ddae8f02042693858c0
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-30 23:19:39 +0300

    day53_StreamPractice

---
+++ src/day53_FunctionalInterface/StreamPractice.java
@@ -0,0 +1,16 @@
+package day53_FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamPractice {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1,1,2,2,3,3,4,4,5,5,6,6));
+
+        list1 = list1.stream().distinct().collect(Collectors.toList());
+
+        System.out.println(list1);
+    }
+}
