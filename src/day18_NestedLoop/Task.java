8a15f3635e18b4bd513b9908fd042c226c253039
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-03 15:21:06 +0300

    day18_NestedLoop

---
+++ src/day18_NestedLoop/Task.java
@@ -0,0 +1,52 @@
+package day18_NestedLoop;
+
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+    /*  boolean one =('a' !='z')&&false;
+        boolean two = one ||0 ==(9-10+1);
+        System.out.println(two);
+
+
+        long l  =100;
+        short s =(short) l;
+        System.out.println(s+10);
+
+
+        int a = 10;
+        a= --a + a++ + a-- + a++;
+        System.out.println(a);
+
+
+        short s=13+3*(10-6)%2;
+        boolean b =s!=25;
+        System.out.println(b);
+
+
+        byte b =104;
+        boolean check =b<100;
+        if(check){
+            b-=100;
+        }else{
+            b=50;
+        }
+        System.out.println(b);
+
+
+        String name ="Jimmy";
+        boolean check;
+        if(name=="Jimmy"){
+            check=false;
+        }else{
+            check=true;
+        }
+        String str=check?"5":"10";
+        System.out.println(str==10);
+
+     */
+
+
+        }
+    }
+
