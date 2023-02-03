9e6ba23c0aeb6e68162c97c2c4f8aa2f3fd04d20
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-03 15:22:39 +0300

    day19_LoopPractices

---
+++ src/day19_LoopPractices/Quiz.java
@@ -0,0 +1,19 @@
+package day19_LoopPractices;
+
+public class Quiz {
+    public static void main(String[] args) {
+       String ta="A";
+       ta=ta.concat("B");
+       String  tb ="C";
+       ta= ta+tb;
+       ta.replace('C','D');
+       ta=ta+tb;
+        System.out.println(ta);
+
+
+        }
+
+
+
+    }
+
