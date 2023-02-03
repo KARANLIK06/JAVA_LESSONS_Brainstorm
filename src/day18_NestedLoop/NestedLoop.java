ceb0492c27fb3e65f656145c9b4c2712e597f870
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-03 15:20:46 +0300

    day18_NestedLoop

---
+++ src/day18_NestedLoop/NestedLoop.java
@@ -0,0 +1,14 @@
+package day18_NestedLoop;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+
+        for (int j = 1; j < 4; j++) {
+            for (int i = 0; i <5 ; i++) {
+                System.out.println("Wooden Spoon");
+
+            }
+
+        }
+    }
+}
