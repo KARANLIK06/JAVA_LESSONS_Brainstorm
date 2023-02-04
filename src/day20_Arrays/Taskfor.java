c6845a35f448024fb6b369d748992da4b750a03d
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-04 17:49:51 +0300

    day20_Arrays

---
+++ src/day20_Arrays/Taskfor.java
@@ -0,0 +1,11 @@
+package day20_Arrays;
+
+public class Taskfor {
+
+    public static void main(String[] args) {
+        String str = " ";
+        str.trim();
+        boolean A = str.isEmpty();
+        System.out.println(A);
+    }
+}
