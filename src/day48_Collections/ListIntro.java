70598a1f87461ee630d8e8d56d28e71e6ee98dde
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-09 10:39:37 +0300

    day21_ForEachLoop

--- src/day48_Collections/ListIntro.java
+++ src/day48_Collections/ListIntro.java
@@ -7,18 +7,23 @@ public class ListIntro {
         List<Integer> list1=new ArrayList<>();
         list1.addAll(Arrays.asList(10,20,30,40,50,60));
         System.out.println(list1.get(0));
+        System.out.println("list1 = " + list1);
 
         List<Integer> list2=new LinkedList<>();
         list2.addAll(Arrays.asList(10,20,30,40,50,60));
         System.out.println(list2.get(0));
+        System.out.println("list2 = " + list2);
 
         List<Integer> list3=new Vector<>();
         list3.addAll(Arrays.asList(10,20,30,40,50,60));
         System.out.println(list3.get(0));
+        System.out.println("list3 = " + list3);
 
         List<Integer> list4=new Stack<>();
         list4.addAll(Arrays.asList(10,20,30,40,50,60));
         System.out.println(list4.get(0));
+        System.out.println("list4 = " + list4);
+
 
         System.out.println("--------------------------");
 
