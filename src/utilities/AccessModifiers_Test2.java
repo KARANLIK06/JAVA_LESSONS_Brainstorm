f38e11e2eb725ccc8b3993ecda92c8614ec98a0a
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-21 00:13:07 +0300

    utilities

---
+++ src/utilities/AccessModifiers_Test2.java
@@ -0,0 +1,15 @@
+package utilities;
+
+import day34_GarbageCollection_AccessModifiers.AccessModifiers;
+
+ class AccessModifiers_Test2 {
+    public static void main(String[] args) {
+        System.out.println(AccessModifiers.publicData);
+       // System.out.println (AccessModifiers.defaultData);    //   default is not visible outside the package
+      //  System.out.println(AccessModifiers.privateData);     //   private is not visible outside the class
+        AccessModifiers.method1();
+      //  AccessModifiers.method2();
+      //  AccessModifiers.method3();
+
+    }
+}
