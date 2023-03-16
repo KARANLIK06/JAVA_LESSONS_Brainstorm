fdc67843928b19701ebe6bfd9e2560452f014655
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-16 13:40:50 +0300

    day28_ArrayList

---
+++ src/day28_ArrayList/Task_7.java
@@ -0,0 +1,28 @@
+package day28_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll( Arrays.asList(1,2,2,3,4,4,5,6,7,7) );
+
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        for (Integer each : list) {
+            if (temp.contains(each)){
+                System.out.println(each);
+                break;
+            }
+            temp.add(each);
+        }
+    }
+}
+/*
+7. Write a program that can return the first duplicated element from an arrayList of Integer
+	Ex:
+		list = [1,2,2,3,4,4,5,6,7,7];
+	output:
+		2
+ */
