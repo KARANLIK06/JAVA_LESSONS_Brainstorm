55d33ee06002612e647b3793f00bfc7d3558cbf9
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-31 10:03:58 +0300

    day31_Constructors

---
+++ src/day31_Constructors/RectangleObjects.java
@@ -0,0 +1,14 @@
+package day31_Constructors;
+
+public class RectangleObjects {
+    public static void main(String[] args) {
+
+        Rectangle  rectangle1 = new Rectangle(5.5,3.5);
+        Rectangle  rectangle2 = new Rectangle(10,6);
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+
+
+
+    }
+}
