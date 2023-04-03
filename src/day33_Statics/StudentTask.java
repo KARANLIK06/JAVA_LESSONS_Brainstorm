f510d626d2f72f0cbb1c1697b89f11cbf53de4a4
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-04 00:51:15 +0300

    day33_Statics

---
+++ src/day33_Statics/StudentTask.java
@@ -0,0 +1,10 @@
+package day33_Statics;
+class StudentClass{
+
+}
+class  Test{
+
+}
+
+public class StudentTask {   //      public class name ile java file name aynı olmalı yani public StudentTask in önüne yazılmalı. Diğer classların önüne yazılınca compile error verir. sadece 1 kere public yazabiliriz.   package içinde aynı class ismi olmaz.(compile error verir)
+}
