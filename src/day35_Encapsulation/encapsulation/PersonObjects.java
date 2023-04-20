a8d35531a3249a2d6f691173ba3267b13493af0d
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-20 23:39:43 +0300

    day35_Encapsulation

---
+++ src/day35_Encapsulation/encapsulation/PersonObjects.java
@@ -0,0 +1,17 @@
+package day35_Encapsulation.encapsulation;
+
+public class PersonObjects {
+    public static void main(String[] args) {
+        Person p1 =new Person();
+
+          //p1.name="Daniella";   //public
+          //p1.age=25;         // public
+        p1.setName("Daniel");
+        p1.setAge(28);
+
+        // System.out.println(p1.name +" : "+p1.age);
+        System.out.println(p1.getName()+" : "+p1.getAge());
+
+
+    }
+}
